97a79959123e23cd184939ad86093c742bd22207
AshrafulAlam <ashraful.alam@brainstation-23.com>
2026-04-15 06:51:37 +0600

    update ndkVersion to 27.0.12077973 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "app.ashraful.taghyeer_technologies.taghyeer_technologies"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
